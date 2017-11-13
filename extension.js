8e50bf9d78ae60b2ba272651668c51fc8aee95c7
Jamie Smith <i@jamiesmiths.com>
2017-11-13 09:14:14 -0500

    added packaged extension; cleaned;

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-var jsonfile = require('jsonfile')
+const jsonfile = require('jsonfile')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,7 +10,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let openSalesforce = vscode.commands.registerCommand('extension.openSalesforce', function () {
+    const openSalesforce = vscode.commands.registerCommand('extension.openSalesforce', function () {
         // The code you place here will be executed every time your command is executed
 
         vscode.workspace.findFiles('force.json').then(x => {
@@ -28,8 +28,9 @@ function activate(context) {
                 const browserName = 'chrome'
                 const path = `${url}?un=${un}&pw=${pw}`
 
-                var exec = require('child_process').exec;
-                
+                const exec = require('child_process').exec;
+                let cmd = ''
+
                 switch (platform) {
                     case 'win32':
                         cmd = browserName
@@ -50,7 +51,7 @@ function activate(context) {
 
                 exec(cmd, function (err, stdout, stderr) {
                     if (err) {
-                        vscode.window.showErrorMessage('error occured!!');
+                        vscode.window.showErrorMessage(`Error: ${err}`);
                     }
                 });
 
